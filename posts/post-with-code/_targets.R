2ddef5f7ccca0579159fe5cd63b765f35a615584
idasitingasine <idasitingassine@gmail.com>
2023-02-28 02:54:36 +0100

    ma deuxieme blog

---
+++ posts/post-with-code/_targets.R
@@ -0,0 +1,12 @@
+library(targets)
+"script"
+
+source("R/fonctions.R")
+
+tar_option_set(packages <- c("heron","quarto","targets"))
+
+
+
+list(tar_target(memo, divide_triangle(0,0,0,1,0.5,sqrt(3/2))),
+     tar_target(catalogue, divide_list_triangle(divide_triangle(0,0,0,1,0.5,sqrt(3/2)))),
+     tar_target(figure, plot_triangle(0,0,0,1,0.5,sqrt(3/2))))
